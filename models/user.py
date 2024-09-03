73aff9c7a250083adf98b186674e245a4edcba8c
BYUUSAA <byuussaa@gmail.com>
2024-09-03 15:48:22 +0100

    user.py

--- models/user.py
+++ models/user.py
@@ -1,18 +1,31 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import unittest
+from models.user import User
 
+class TestUser(unittest.TestCase):
+    def test_instantiation(self):
+        user = User()
+        self.assertIsInstance(user, User)
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    def test_attributes(self):
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
+        self.assertTrue(hasattr(user, 'password'))
+        self.assertTrue(hasattr(user, 'first_name'))
+        self.assertTrue(hasattr(user, 'last_name'))
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    def test_save(self):
+        user = User()
+        user.save()
+        self.assertTrue(user.updated_at > user.created_at)
 
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    def test_to_dict(self):
+        user = User()
+        user_dict = user.to_dict()
+        self.assertIsInstance(user_dict, dict)
+        self.assertIn('id', user_dict)
+        self.assertIn('created_at', user_dict)
+        self.assertIn('updated_at', user_dict)
+        self.assertIn('email', user_dict)
+        self.assertIn('password', user_dict)
+        self.assertIn('first_name', user_dict)
+        self.assertIn('last_name', user_dict)
