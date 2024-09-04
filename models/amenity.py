fa422e2e6ebf3c81f413baabe9f3e2ffb63672de
BYUUSAA <byuussaa@gmail.com>
2024-09-04 15:55:06 +0100

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project This file defines the Amenity Model
-It inherits from the BaseModel
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
-    """The Amenity Model to store amenity information"""
+
+    """Amenity class to store amenity information"""
 
     __tablename__ = "amenities"
-    
-     # Attributes
-    name = ""
+
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity")
-
