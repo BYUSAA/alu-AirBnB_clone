3794afe5fd03a3e053f0b6b1183d997f14469666
BYUUSAA <byuussaa@gmail.com>
2024-09-04 09:50:48 +0100

    console.py

--- console.py
+++ console.py
@@ -1,9 +1,46 @@
 #!/usr/bin/python3
 import cmd
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for managing AirBnB objects."""
-    prompt = '(hbnb) '
+    """Command processor for the HBNB console."""
+
+    prompt = "(hbnb) "
+    classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'Place': Place,
+        'Review': Review,
+        # other classes...
+    }
+
+    def do_create(self, args):
+        """Creates a new instance of a class."""
+        if not args:
+            print("** class name missing **")
+            return
+        if args not in self.classes:
+            print("** class doesn't exist **")
+            return
+        obj = self.classes[args]()
+        obj.save()
+        print(obj.id)
+
+    def do_show(self, args):
+        """Shows the string representation of an instance."""
+        # code to show...
+
+    def do_destroy(self, args):
+        """Destroys an instance based on the class name and id."""
+        # code to destroy...
+
+    def do_all(self, args):
+        """Shows all instances of a class or all instances if no class is specified."""
+        # code to display all...
+
+    def do_update(self, args):
+        """Updates an instance based on the class name and id by adding or updating attribute."""
+        # code to update...
 
     def do_quit(self, arg):
         """Quit the command interpreter."""
@@ -20,3 +57,13 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
+
+
+
+
+
+
+
+
+
