a398a5bbb19969da246552b4b372d5b9e9a86fac
BYUUSAA <byuussaa@gmail.com>
2024-09-04 15:28:42 +0100

    state.py

--- models/state.py
+++ models/state.py
@@ -8,10 +8,12 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
 
-"""This is the class for State Attributes: name: input name The State Model"""
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
-    # Attributes
-    name = ""
-    name = Column(String(125), nullable=False)
+
+    name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
