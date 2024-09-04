33c9d3053523cb3077bcd3844d6ec5cca6e4c9da
BYUUSAA <byuussaa@gmail.com>
2024-09-04 15:57:33 +0100

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,83 +1,90 @@
-#!/usr/bin/python3
-""" Database storage module """
-from sqlalchemy import create_engine
-# import declarative base
-from sqlalchemy.orm import sessionmaker, scoped_session
+#!/usr/bin/env python
+"""A module that that serializes instances to a JSON file and deserializes
+JSON file to instances"""
+
+import json
 import os
+from models.base_model import BaseModel
+from models.user import User
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.city import City
-from models.user import User
-from models.base_model import Base
 
 
-class DBStorage:
+class FileStorage:
 
     # private class attributes
-    __engine = None
-    __session = None
+    # __file_path is the path to the JSON file to store all objects.
+    __file_path = 'storage.json'
 
-    def __init__(self):
-
-        # get environment variables
-        user = os.getenv('HBNB_MYSQL_USER')
-        passwd = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        db = os.getenv('HBNB_MYSQL_DB')
-        env = os.getenv('HBNB_ENV')
-
-        db_path = ('mysql+mysqldb://{}:{}@{}/{}'
-                   .format(user, passwd, host, db))
-
-        self.__engine = create_engine(db_path, pool_pre_ping=True)
-        # drop all tables if the environment variable HBNB_ENV is equal to test
-        if env == 'test':
-            Base.metadata.drop_all(self.__engine)
+    # __objects is a dictionary that stores all objects by <class name>.id
+    # ex: to store a BaseModel object with id=12121212, the key will be
+    # BaseModel.12121212 and the value will be the object.
+    # the object (value of key) is stored like this:
+    # <models.base_model.BaseModel object at 0x7f3329dac310>
+    # obects = {BaseModel.12121212: }
+    __objects = {}
 
     def all(self, cls=None):
-        """ query on the current database session """
-        # create a dictionary
-        obj_dict = {}
-        if cls is None:
-            classes = [State, City, User, Place, Review, Amenity]
-            for class_name in classes:
-                for obj in self.__session.query(class_name):
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    obj_dict[key] = obj
+        """Returns a list of all objects if cls is None. If cls is provided, return all objects of that type.
+        """
+
+        if cls is not None:
+
+            obj = {}
+            print(FileStorage.__objects.items())
+            for key, val in FileStorage.__objects.items():
+                if cls.__name__ in key:
+                    obj[key] = val
+            return obj
         else:
-            for obj in self.__session.query(cls):
-                key = obj.__class__.__name__ + '.' + obj.id
-                obj_dict[key] = obj
-        return obj_dict
+            return self.__objects
 
+    # sets in __objects the obj with key <obj class name>.id
     def new(self, obj):
-        """ add the object to the current database session """
-        self.__session.add(obj)
+        """Add obj with key <obj class name>.id to dictionary.
 
+        Args:
+
+        obj: the object with key <obj class name>.id
+        """
+        key = obj.__class__.__name__ + '.' + obj.id
+        # json_data = json.dump(obj)
+        self.__objects[key] = obj
+
+    # serializes __objects to the JSON file (path: __file_path)
     def save(self):
-        """ commit all changes of the current database session """
-        try:
-            self.__session.commit()
-        except Exception:
-            self.__session.rollback()
-        finally:
-            self.__session.close()
+        """ Serializes __objects to the JSON file (path: __file_path)."""
+        json_obj = {}
+        for key in self.__objects.keys():
+            json_obj[key] = self.__objects[key].to_dict()
 
-    def delete(self, obj=None):
-        """ delete from the current database session obj if not None """
-        if obj is not None:
-            self.__session.delete(obj)
+        with open(self.__file_path, 'w') as json_file:
+            json.dump(json_obj, json_file)
 
     def reload(self):
-        """ create all tables in the database """
-        Base.metadata.create_all(self.__engine)
-        # create a configured "Session" class
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
-        # create a Session
-        self.__session = Session()
+        """Deserializes the JSON file to __objects (only if the JSON file"""
+        """(path: __file_path) exists ; otherwise, do nothing."""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as json_file:
+                json_obj = json.load(json_file)
+                for key in json_obj.keys():
 
+                    # By providing the dict value stored in json_obj[key] as
+                    # kwargs, genrate an object with the same attributes
+                    self.__objects[key] = eval(
+                        json_obj[key]['__class__'])(**json_obj[key])
+
+    def delete(self, obj=None):
+        """Delete an object from the __objects"""
+        if obj is not None:
+            for key, val in list(FileStorage.__objects.items()):
+                if obj == val:
+                    del FileStorage.__objects[key]
+                    print("Deleted: {}".format(key))
+                    self.save()
+                    
     def close(self):
-        self.__session.close()
+        self.reload()
