e9c0d037011bf36ee735ef5c72c65dc8cb16ba1a
BYUUSAA <byuussaa@gmail.com>
2024-09-04 09:39:05 +0100

    review.py

--- models/review.py
+++ models/review.py
@@ -6,13 +6,28 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 
 class Review(BaseModel, Base):
     """Review class to store review information
+    
 
     Args:
         BaseModel (_type_): _description_
         Base (_type_): _description_
     """
+
+    """Review module for the HBNB project"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Review class to store review information"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    
     __tablename__ = "reviews"
 
     place_id = Column(ForeignKey('places.id'), nullable=False)
     user_id = Column(ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
+
+
